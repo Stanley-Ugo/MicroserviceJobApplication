608278beba5771f6e44c848c87eacdac9daf3821
Stanley-Ugo <iamkosmicblast@gmail.com>
2024-05-29 20:54:35 +0100

    Adding docker-compose.yaml file

--- jobms/src/main/java/com/embarkx/jobms/job/Impl/JobServiceImpl.java
+++ jobms/src/main/java/com/embarkx/jobms/job/Impl/JobServiceImpl.java
@@ -3,8 +3,12 @@ package com.embarkx.jobms.job.Impl;
 import com.embarkx.jobms.job.Job;
 import com.embarkx.jobms.job.JobRepository;
 import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.dto.JobWithCompanyDTO;
+import com.embarkx.jobms.job.external.Company;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +22,24 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
-    public List<Job> findAll() {
-        return jobRepository.findAll();
+    public List<JobWithCompanyDTO> findAll() {
+        List<Job> jobs = jobRepository.findAll();
+        List<JobWithCompanyDTO> jobWithCompanyDTOs = new ArrayList<>();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        for (Job job : jobs){
+            JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+            jobWithCompanyDTO.setJob(job);
+
+            Company company = restTemplate.getForObject("http://localhost:8081/companies/" + job.getCompanyId(),
+                    Company.class);
+            jobWithCompanyDTO.setCompany(company);
+
+            jobWithCompanyDTOs.add(jobWithCompanyDTO);
+        }
+
+        return jobWithCompanyDTOs;
     }
 
     @Override

1d1487e44ac5ae43345761e7276b78b5b6a117bc
Stanley-Ugo <iamkosmicblast@gmail.com>
2024-05-29 21:43:16 +0100

    Adding service-reg project for service descovery

--- jobms/src/main/java/com/embarkx/jobms/job/Impl/JobServiceImpl.java
+++ jobms/src/main/java/com/embarkx/jobms/job/Impl/JobServiceImpl.java
@@ -8,9 +8,9 @@ import com.embarkx.jobms.job.external.Company;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class JobServiceImpl implements JobService {
@@ -24,22 +24,9 @@ public class JobServiceImpl implements JobService {
     @Override
     public List<JobWithCompanyDTO> findAll() {
         List<Job> jobs = jobRepository.findAll();
-        List<JobWithCompanyDTO> jobWithCompanyDTOs = new ArrayList<>();
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        for (Job job : jobs){
-            JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
-            jobWithCompanyDTO.setJob(job);
-
-            Company company = restTemplate.getForObject("http://localhost:8081/companies/" + job.getCompanyId(),
-                    Company.class);
-            jobWithCompanyDTO.setCompany(company);
-
-            jobWithCompanyDTOs.add(jobWithCompanyDTO);
-        }
-
-        return jobWithCompanyDTOs;
+        return jobs.stream().map(this::convertToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -76,5 +63,16 @@ public class JobServiceImpl implements JobService {
             return true;
         }
         return false;
+    }
+
+    private JobWithCompanyDTO convertToDto(Job job){
+        JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+        jobWithCompanyDTO.setJob(job);
+        RestTemplate restTemplate = new RestTemplate();
+        Company company = restTemplate.getForObject("http://localhost:8081/companies/" + job.getCompanyId(),
+                Company.class);
+        jobWithCompanyDTO.setCompany(company);
+
+        return jobWithCompanyDTO;
     }
 }

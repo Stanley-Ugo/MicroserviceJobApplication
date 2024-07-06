bd884e6164a4ecbfbad4c6848e9898a17b4d50bc
Stanley-Ugo <iamkosmicblast@gmail.com>
2024-07-06 23:40:30 +0100

    Adding OpenFeign Company and Review Clients

--- jobms/src/main/java/com/embarkx/jobms/job/Impl/JobServiceImpl.java
+++ jobms/src/main/java/com/embarkx/jobms/job/Impl/JobServiceImpl.java
@@ -3,6 +3,8 @@ package com.embarkx.jobms.job.Impl;
 import com.embarkx.jobms.job.Job;
 import com.embarkx.jobms.job.JobRepository;
 import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.clients.CompanyClient;
+import com.embarkx.jobms.job.clients.ReviewClient;
 import com.embarkx.jobms.job.dto.JobDTO;
 import com.embarkx.jobms.job.external.Company;
 import com.embarkx.jobms.job.external.Review;
@@ -26,8 +28,13 @@ public class JobServiceImpl implements JobService {
     @Autowired
     RestTemplate restTemplate;
 
-    public JobServiceImpl(JobRepository jobRepository) {
+    private CompanyClient companyClient;
+    private ReviewClient reviewClient;
+
+    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
         this.jobRepository = jobRepository;
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
     }
 
     @Override
@@ -78,15 +85,8 @@ public class JobServiceImpl implements JobService {
     }
 
     private JobDTO convertToDto(Job job){
-        Company company = restTemplate.getForObject("http://COMPANY-SERVICE:8081/companies/" + job.getCompanyId(),
-                Company.class);
-
-        ResponseEntity<List<Review>> reviewResponse = restTemplate.exchange("http://REVIEW-SERVICE:8083/reviews?companyId=" + job.getCompanyId(),
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Review>>() {});
-
-        List<Review> reviews = reviewResponse.getBody();
+        Company company = companyClient.getCompany(job.getCompanyId());
+        List<Review> reviews = reviewClient.getReviews(company.getId());
 
         return JobMapper.mapToJobWIthCompanyDto(job, company, reviews);
     }
